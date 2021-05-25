787261a3983f1b4bb99a97e4e79992c94dbe517a
Marcel Milcent <marcelmilcent@gmail.com>
2021-05-25 19:06:21 -0300

    benfordviz BenfordBokehChart - first successful

--- benford_analysis.py
+++ benford_analysis.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd
+from bokeh.plotting import figure
+from bokeh.models import Tabs, Panel
+from bokeh.layouts import Column
+
 import benford as bf
+from benfordviz.bokeh_plotting import BenfordBokehChart
 
 csv_file = st.sidebar.file_uploader("", type="csv",
                 key="csv_uploader")
 try:
     df = pd.read_csv(csv_file)
-    st.dataframe(df)
+    # st.dataframe(df)
 except:
     pass
 
@@ -24,12 +29,16 @@ try:
     bo = bf.Benford(df[col], decimals=decimals, sign=sign[:3])
     st.sidebar.markdown(f"#### Initial sample size: **{len(df)}**.")
     st.sidebar.markdown(f"#### Analysis performed on **{len(bo.base)}** " +
-                        f"non-zero and {sign}-sign records.")
+                        f"non-zero, {sign}-sign records.")
     st.sidebar.markdown("#### Number of discarded entries for each test " +
                         " (dependent on decimal places chosen): ")
     st.sidebar.markdown("- " +"\n - ".join(
-                            [f"{bf.constants.names[key]}: **{val}**" for key, 
+                            [f"{bf.constants.TEST_NAMES[key]}: **{val}**" for key, 
                             val in bo._discarded.items()]))
 except:
     st.markdown("#### Waiting for proper column selection.")
 
+# st.bokeh_chart(tabs)
+bbc = BenfordBokehChart(bo.F2D)
+# st.write(type(bbc))
+st.bokeh_chart(bbc.figure)
