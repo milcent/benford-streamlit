7aea3ba9f9ef0d149b7a35a342d61710296d59c5
Marcel Milcent <marcelmilcent@gmail.com>
2021-05-27 08:39:41 -0300

    confidence slider and layout rearrangements

--- benford_analysis.py
+++ benford_analysis.py
@@ -5,6 +5,7 @@ from bokeh.models import Tabs, Panel
 from bokeh.layouts import Column
 
 import benford as bf
+from benford.constants import CONFS
 from benfordviz.bokeh_plotting import BenfordBokehChart
 
 csv_file = st.sidebar.file_uploader("", type="csv",
@@ -15,24 +16,32 @@ try:
 except:
     pass
 
-col = st.selectbox("==== Select column for analysis, whose dtype is 'int', "
+col = st.sidebar.selectbox("==== Select column for analysis, whose dtype is 'int', "
                     "'float', or an easily convertible string. ====", df.columns)
 
 # st.write(df.dtypes)
-decimals = st.sidebar.number_input("Number of decimal places", min_value=0,
-                        value=2, step=1)
-
 sign = st.sidebar.selectbox("Choose which records' sign to analyse",
                     options=["all", "positive", "negative"])
 
+decimals = st.sidebar.number_input("Number of decimal places", min_value=0,
+                        value=2, step=1)
+
+confidence = st.sidebar.select_slider(
+    "Choose confidence level (%)", options=list(CONFS.keys())
+)
+if not (isinstance(confidence, int) | isinstance(confidence, float)):
+    confidence=None
+
 try:
-    bo = bf.Benford(df[col], decimals=decimals, sign=sign[:3])
-    st.sidebar.markdown(f"#### Initial sample size: **{len(df)}**.")
-    st.sidebar.markdown(f"#### Analysis performed on **{len(bo.base)}** " +
+    bo = bf.Benford(
+        df[col], decimals=decimals, sign=sign[:3], confidence=confidence
+    )
+    st.markdown(f"#### Initial sample size: **{len(df)}**.")
+    st.markdown(f"#### Analysis performed on **{len(bo.base)}** " +
                         f"non-zero, {sign}-sign records.")
-    st.sidebar.markdown("#### Number of discarded entries for each test " +
+    st.markdown("#### Number of discarded entries for each test " +
                         " (dependent on decimal places chosen): ")
-    st.sidebar.markdown("- " +"\n - ".join(
+    st.markdown("- " +"\n - ".join(
                             [f"{bf.constants.TEST_NAMES[key]}: **{val}**" for key, 
                             val in bo._discarded.items()]))
 except:
