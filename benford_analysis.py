d56257b06889234766f012e4bf1ab98c02cd2167
Marcel Milcent <marcelmilcent@gmail.com>
2021-06-01 16:13:14 -0300

    GNU GPL V3 license

--- benford_analysis.py
+++ benford_analysis.py
@@ -1,3 +1,22 @@
+"""
+Marcel Milcent, Copyright 2021
+
+This file is part of benford-streamlit app.
+
+    benford-streamlit app is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    benford-streamlit app is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with benford-streamlit app.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 import streamlit as st
 
 import benford as bf
